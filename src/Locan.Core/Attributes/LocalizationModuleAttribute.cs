c5398c3126c10b90176480f23fa6724c571aba77
Diyar Tashliyan <oyikavamiko@gmail.com>
2026-08-31 17:46:22 +0300

    create core project and editor config

---
+++ src/Locan.Core/Attributes/LocalizationModuleAttribute.cs
@@ -0,0 +1,24 @@
+namespace Locan.Core.Attributes;
+
+/// <summary>
+/// Specifies assembly that is used for localization source generation
+/// </summary>
+[AttributeUsage(AttributeTargets.Assembly)]
+public sealed class LocalizationModuleAttribute : Attribute
+{
+    /// <param name="name">The module name</param>
+    public LocalizationModuleAttribute(string name)
+    {
+        Name = name;
+    }
+    
+    /// <summary>
+    /// Gets module name
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// Specifies if the generated module shall be internal.
+    /// </summary>
+    public bool IsInternal { get; set; }
+}
